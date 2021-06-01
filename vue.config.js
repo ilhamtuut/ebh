7d0796951157639b30dba946b5c044739708bae6
ilhamtuut <ilham.ahmad14@gmail.com>
2021-06-01 10:23:26 +0700

    config vue

--- vue.config.js
+++ vue.config.js
@@ -2,7 +2,7 @@ const path = require("path");
 
 module.exports = {
   publicPath:
-    process.env.NODE_ENV === "production" ? "/ebh/" : "/",
+    process.env.NODE_ENV === "production" ? "/" : "/",
   configureWebpack: {
     resolve: {
       alias: {
